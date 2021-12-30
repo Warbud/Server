412fec6aa3e62e037649fda34d8d7dedf57ddb52
Adrian Franczak <44712992+Nairda015@users.noreply.github.com>
2021-12-30 15:39:07 +0100

    Add status code to exceptions

--- Warbud.Revit.Statistics/Exceptions/StatisticNotFoundException.cs
+++ Warbud.Revit.Statistics/Exceptions/StatisticNotFoundException.cs
@@ -1,9 +1,12 @@
-﻿using Warbud.Shared.Abstraction.Exceptions;
+﻿using System.Net;
+using Warbud.Shared.Abstraction.Exceptions;
 
 namespace Warbud.Revit.Statistics.Exceptions
 {
     public class StatisticNotFoundException : WarbudException
     {
+        public override HttpStatusCode StatusCode => HttpStatusCode.NotFound;
+
         public StatisticNotFoundException() : base("The requested element was not found.")
         {
         }
