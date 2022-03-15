0c19941c5d0f337c982bbe5dbc5caf1670db1c83
Adrian Franczak <44712992+Nairda015@users.noreply.github.com>
2022-03-15 18:19:10 +0100

    Update config

--- Warbud.Revit.Statistics/build.sh
+++ Warbud.Revit.Statistics/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 warbudHome="C:/WEBSITES/Warbud"
+folderPath="Builds/RevitStats"
 
-dotnet publish -c Release -o $warbudHome/Builds/RevitStats
+rm -r $warbudHome/$folderPath
+dotnet publish -c Release -o $warbudHome/$folderPath
