61c708df00bea2948b6d167352a74844c60570e4
Adrian Franczak <44712992+Nairda015@users.noreply.github.com>
2022-03-31 16:33:29 +0200

    Update ribbon version

--- Warbud.Revit.Statistics/Entities/Statistic.cs
+++ Warbud.Revit.Statistics/Entities/Statistic.cs
@@ -17,11 +17,12 @@ namespace Warbud.Revit.Statistics.Entities
         {
             UserByVariables = userByVariables;
             OperationData = operationData;
+            DateTime = DateTime.Now;
         }
 
         public int Id { get; private  set;}
         internal UserByVariables UserByVariables { get; private set;}
         internal OperationData OperationData { get; private set; }
-        internal DateTime DateTime { get; private set; } = DateTime.Now;
+        internal DateTime DateTime { get; private set; }
     }
 }
